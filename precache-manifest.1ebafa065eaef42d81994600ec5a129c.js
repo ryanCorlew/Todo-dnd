511b69fc30ab23c67688f9fb0d567e4b326695a7
Ryan Corlew <ryancorlew13@gmail.com>
2020-05-25 13:27:42 -0400

    Updates

---
+++ precache-manifest.1ebafa065eaef42d81994600ec5a129c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76aa8e0acfc1416a4926d6a7b6f555f3",
+    "url": "/Todo-dnd/index.html"
+  },
+  {
+    "revision": "2ea3dc182b5831a93e7c",
+    "url": "/Todo-dnd/static/css/main.f27b75cd.chunk.css"
+  },
+  {
+    "revision": "10b36873f298975afcf8",
+    "url": "/Todo-dnd/static/js/2.355cf106.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todo-dnd/static/js/2.355cf106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ea3dc182b5831a93e7c",
+    "url": "/Todo-dnd/static/js/main.c703a64e.chunk.js"
+  },
+  {
+    "revision": "d5a5837f58ef9680eaf0",
+    "url": "/Todo-dnd/static/js/runtime-main.15d1b57d.js"
+  }
+]);
